35313edb49faccf4f96ac28c860dc73d44bf3c68
nasrin jafari <nasrin.jafari770@gmail.com>
2023-09-18 22:11:34 +0430

    design section Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,7 +3,10 @@ import styles from "../style";
 import FeedbackCard from "./FeedbackCard";
 
 const Testimonials = () => (
-  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+  <section
+    id="clients"
+    className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}
+  >
     <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
@@ -17,9 +20,10 @@ const Testimonials = () => (
         </p>
       </div>
     </div>
-
     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
-      {feedback.map((card) => <FeedbackCard key={card.id} {...card} />)}
+      {feedback.map((card) => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
     </div>
   </section>
 );
